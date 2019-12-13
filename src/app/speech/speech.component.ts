5d53a8a6e204cc45ee87c3e69cff746e1cbd2b4f
akhilrana0001 <akhilrana0001@gmail.com>
2019-12-14 03:02:47 +0530

    master

--- src/app/speech/speech.component.ts
+++ src/app/speech/speech.component.ts
@@ -13,20 +13,66 @@ export class SpeechComponent implements OnInit {
   constructor(public as: AppService) {
     SpeechComponent.speechInput = this.speechInput1;
     SpeechComponent.as = as;
-    // var synth = window.speechSynthesis;
-    // var voices = synth.getVoices();
-    // // var toSpeak = new SpeechSynthesisUtterance(txtInput.value);
 
-    // synth.speak(new SpeechSynthesisUtterance("Hello, User"));
-    // // var message = document.querySelector("#message");
-    speechSynthesis.speak(new SpeechSynthesisUtterance("hello world"));
+    speechSynthesis.speak(
+      new SpeechSynthesisUtterance(
+        "Hello user, Welcome to google voice search!, click on the mic to start or say 'search'"
+      )
+    );
+    setTimeout(function() {
+      SpeechComponent.inputSearch();
+    }, 6000);
   }
 
   ngOnInit() {}
   speechInput1;
   static speechInput;
 
-  public voiceInput() {
+  static inputSearch() {
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+
+    var grammar = "#JSGF V1.0;";
+    var recognition = new SpeechRecognition();
+    var speechRecognitionList = new SpeechGrammarList();
+    speechRecognitionList.addFromString(grammar, 1);
+    recognition.grammars = speechRecognitionList;
+    recognition.continuous = true;
+    recognition.lang = "en-IN";
+    recognition.interimResults = false;
+
+    recognition.onresult = function(event) {
+      var last = event.results.length - 1;
+      var command = event.results[last][0].transcript;
+      // message.textContent = "Voice Input: " + command + ".";
+      // console.log(command);
+      if (command == "search") {
+        var sound = new Audio();
+        sound.src = "assets/sounds/didong.mp3";
+        sound.play();
+        SpeechComponent.voiceInput1();
+      }
+
+      // gSearch;
+    };
+
+    recognition.onspeechend = function() {
+      recognition.stop();
+    };
+    recognition.onaudiostart = function() {
+      // console.log("sound");
+      var sound = new Audio();
+      sound.src = "assets/sounds/didong.mp3";
+      sound.play();
+    };
+
+    recognition.onerror = function(event) {
+      console.log("Error occurred in recognition: " + event.error);
+    };
+
+    recognition.start();
+  }
+  static voiceInput1() {
     var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
     var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 
@@ -62,6 +108,9 @@ export class SpeechComponent implements OnInit {
     };
     recognition.onaudiostart = function() {
       // console.log("sound");
+      var sound = new Audio();
+      sound.src = "assets/sounds/didong.mp3";
+      sound.play();
       setTimeout(function() {
         (<HTMLInputElement>(
           document.getElementById("voiceButton")
@@ -74,6 +123,13 @@ export class SpeechComponent implements OnInit {
     };
 
     recognition.onerror = function(event) {
+      (<HTMLInputElement>(
+        document.getElementById("voiceButton")
+      )).style.background = "RGBA(255, 64, 129, 1)";
+
+      (<HTMLInputElement>(
+        document.getElementById("voiceButton")
+      )).disabled = false;
       console.log("Error occurred in recognition: " + event.error);
     };
 
@@ -93,5 +149,8 @@ export class SpeechComponent implements OnInit {
   }
   gSearch1() {
     SpeechComponent.gSearch();
+  }
+  voiceInput() {
+    SpeechComponent.voiceInput1();
   }
 }
