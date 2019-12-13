fcb8054df3593f50b90908aa77fcde0827a00f1b
akhilrana0001 <akhilrana0001@gmail.com>
2019-12-13 23:44:37 +0530

    master

--- server.js
+++ server.js
@@ -5,7 +5,29 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/speech-google"));
 
