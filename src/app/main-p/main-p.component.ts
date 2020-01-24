5625877e298502afd210696d4056a91970bb8a75
akhilrana0001 <akhilrana0001@gmail.com>
2020-01-24 17:50:28 +0530

    websight

--- src/app/main-p/main-p.component.ts
+++ src/app/main-p/main-p.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+declare var webkitSpeechGrammarList: any;
+declare var webkitSpeechRecognition: any;
 
 @Component({
-  selector: 'app-main-p',
-  templateUrl: './main-p.component.html',
-  styleUrls: ['./main-p.component.css']
+  selector: "app-main-p",
+  templateUrl: "./main-p.component.html",
+  styleUrls: ["./main-p.component.css"]
 })
 export class MainPComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
